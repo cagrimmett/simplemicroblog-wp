7a87dad80924d91041993902007493976b1bc9f5
Chuck Grimmett <cagrimmett@gmail.com>
2017-05-05 13:38:57 -0400

    made sidebar titles h3 instead of h2

--- functions.php
+++ functions.php
@@ -95,8 +95,8 @@ function micro_widgets_init() {
 		'description'   => esc_html__( 'Add widgets here.', 'micro' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
 	) );
 	register_sidebar( array(
 		'name' => 'Footer widgets',
